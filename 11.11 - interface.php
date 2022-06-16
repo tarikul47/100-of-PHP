510edb02c297a2d37dbb5c6185d7ba425a09d033
Tarikul <tarikul47@gmail.com>
2022-06-16 16:50:45 +0600

    interface update

--- 11.11 - interface.php
+++ 11.11 - interface.php
@@ -1,40 +1,46 @@
 <?php
 /**
- * interface may not include a varaible
+ * Many interface we can include in a class but interface can't it. 
+ * interface can't include varaibles but only keep method 
  * not need access type modifieer [protectet, private]
- * public can keep
- * not call object instance 
- * inheritance vs interface 
- *  - inheritance = we can inherite from one class 
- *  -interface =  we can implements from multiple interface 
+ * public method only suport 
+ * Interface may not need classs interface just it can implement in another class
+ * inheritance vs interface
+ *  - inheritance = we can inherite from one class
+ *  -interface =  we can implements from multiple interface
  * source = https://www.youtube.com/watch?v=qOzQbnDaMng
+ * 
+ * For example: 
+ *  - Here we have two interface for adding and substruction method 
+ *  - the two method we can implement in our Calulation class 
  */
 
-interface parent1
+interface Add
 {
-    public function calc($a, $b);
+    public function addCalc($param1, $param2);
 }
 
-interface parent2
+interface Sub
 {
-    function sub($c, $d);
+    function subCal($param3, $param4);
 }
 
 // drive clsss
 
-class ChildClass implements parent1, parent2
+class Calulation implements Add, Sub
 {
-    public function calc($a, $b)
+    public function addCalc($a, $b)
     {
         echo $a + $b;
     }
-    public function sub($c, $d)
+    public function subCal($c, $d)
     {
         echo $c + $d;
     }
 }
 
-$cal = new ChildClass();
-$cal->calc(5,6);
 
-echo $cal instanceof parent1;
+$cal = new Calulation();
+$cal->addCalc(5, 6);
+
+//echo $cal instanceof parent1;
